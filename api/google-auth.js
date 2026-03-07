0244893c12bb3e071cf5f91fb372105606bf3c55
martinhogsgaard <mh@weproduce.dk>
2026-03-07 16:33:23 +0100

    Update google-auth.js

--- api/google-auth.js
+++ api/google-auth.js
@@ -6,7 +6,8 @@ export default function handler(req, res) {
     'https://www.googleapis.com/auth/calendar',
     'https://www.googleapis.com/auth/calendar.events',
     'https://www.googleapis.com/auth/gmail.send',
-    'https://www.googleapis.com/auth/gmail.readonly'
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/contacts.readonly'
   ].join(' ');
 
   const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
