3ca9aa986e86c4fd00bddd699257d896af76e828
martinhogsgaard <mh@weproduce.dk>
2026-03-08 13:24:37 +0100

    Update drive.js

--- api/drive.js
+++ api/drive.js
@@ -70,7 +70,7 @@ async function getSheetText(fileId, headers) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://vox-ten-iota.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
